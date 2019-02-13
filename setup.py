b82724634b659b25c73778729a2c16fc490ab920
englehardt <englehardt@gmail.com>
2019-02-13 14:11:32 -0800

    Add init file to package

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     name='trackingprotection-tools',
     license='MPL 2.0',
     url='https://github.com/mozilla/trackingprotection-tools',
-    version='0.1.1',
+    version='0.1.2',
 
     # Dependencies
     install_requires=requirements,
