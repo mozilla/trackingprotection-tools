d940145a23c648c67782068f29aea463a577d89c
Stefan Zabka <zabkaste@informatik.hu-berlin.de>
2020-06-19 12:19:00 +0200

    Removed python2

--- setup.py
+++ setup.py
@@ -29,17 +29,15 @@ setup(
 
     # Classifiers
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 3',
         'Environment :: Web Environment :: Mozilla',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Scientific/Engineering :: Information Analysis'
     ],
