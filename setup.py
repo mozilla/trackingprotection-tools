9e6922eba0328bd5b8918b4ecd54282d4d73edac
stefan <h_dieter@mail.ru>
2020-10-05 16:40:30 +0200

    Bumped version to 0.6.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     name='trackingprotection-tools',
     license='MPL 2.0',
     url='https://github.com/mozilla/trackingprotection-tools',
-    version='0.5.0',
+    version='0.6.0',
     packages=['trackingprotection_tools'],
 
     # Dependencies
